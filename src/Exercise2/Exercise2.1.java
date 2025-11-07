9c60dc97efafa188fa81dfd68f866fcbadce7b80
Bui Sy Tuan <bst0843@gmail.com>
2025-11-07 11:25:31 +0700

    baitap

--- src/Exercise2/Exercise2.1.java
+++ src/Exercise2/Exercise2.1.java
@@ -1,8 +1,9 @@
-public class Author {
+public class Test {
     private String name;
     private String email;
     private char gender;
-    public Author(String name, String email, char gender) {
+
+    public Test(String name, String email, char gender) {
         this.name = name;
         this.email = email;
         this.gender = gender;
@@ -16,16 +17,11 @@ public class Author {
     public char getGender() {
         return gender;
     }
-    public void setName(String name) {
-        this.name = name;
-    }
     public void setEmail(String email) {
         this.email = email;
     }
-    public void setGender(char gender) {
-        this.gender = gender;
-    }
-    public String toString(){
-        return "Author[ten=" + name + ", email=" + email + ", gioiTinh=" + gender + "]";
+    public String toString() {
+        return "[Name: " + name + ", Email: " + email + ", Gender: " + gender + "]";
     }
 }
+
