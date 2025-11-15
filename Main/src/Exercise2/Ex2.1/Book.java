0c172487ed8e69a2036d4b294bb58875b4d2174d
Bui Sy Tuan <bst0843@gmail.com>
2025-11-15 13:20:29 +0700

    baitap

---
+++ Main/src/Exercise2/Ex2.1/Book.java
@@ -0,0 +1,52 @@
+public class Book {
+    private String name;
+    public Author author;
+    public double price;
+    private int qty;
+
+    public Book(String name, Author author, double price) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+
+    public Book(String name, Author author, double price, int qty) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.qty = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    @Override
+    public String toString() {
+        return "Book[" +
+                "name='" + name + '\'' + ", " + author +
+                ", price=" + price +
+                ", qty=" + qty +
+                ']';
+    }
+}
---
+++ Main/src/Exercise2/Ex2.5/Account.java
@@ -0,0 +1,56 @@
+public class Account {
+    private int id;
+    private Customer customer;
+    private double balance;
+
+    public Account(int id, Customer customer, double balance) {
+        this.id = id;
+        this.customer = customer;
+        this.balance = 0.0;
+    }
+
+    public Account(int id, Customer customer) {
+        this.id = id;
+        this.customer = customer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerName() {
+        return customer.getName();
+    }
+
+    public Account deposit(double amount) {
+        this.balance += amount;
+        return this;
+    }
+
+    public Account withdraw(double amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+            return this;
+        }
+        else {
+            return null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return customer.toString() + " " + "balance=" + balance;
+    }
+}
